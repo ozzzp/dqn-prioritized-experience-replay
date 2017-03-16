cef70751919dedea910dbc7c9c0e4cb467c622b9
Evalds Urtans <evalds.urtans@gmail.com>
2017-03-16 13:24:47 +0200

    Simplified version

--- rank_based.py
+++ rank_based.py
@@ -17,7 +17,6 @@ class Experience(object):
     def __init__(self, conf):
         self.size = conf['size']
         self.replace_flag = conf['replace_old'] if 'replace_old' in conf else True
-        self.priority_size = conf['priority_size'] if 'priority_size' in conf else self.size
 
         self.alpha = conf['alpha'] if 'alpha' in conf else 0.7
         self.beta_zero = conf['beta_zero'] if 'beta_zero' in conf else 0.5
@@ -26,60 +25,23 @@ class Experience(object):
 
         # http://www.evernote.com/l/ACnDUVK3ShVEO7fDm38joUGNhDik3fFaB5o/
         self.total_steps = conf['steps'] if 'steps' in conf else 100000
-        # partition number N, split total size to N part
-        self.partition_num = conf['partition_num'] if 'partition_num' in conf else 5
 
         self.index = 0
         self.record_size = 0
         self.isFull = False
 
         self._experience = {}
-        self.priority_queue = binary_heap.BinaryHeap(self.priority_size)
-        self.distributions = self.build_distributions()
+        self.priority_queue = binary_heap.BinaryHeap(self.size)
+
+        # P(i) = (rank i) ^ (-alpha) / sum ((rank i) ^ (-alpha))
+        pdf = list(
+            map(lambda x: math.pow(x, -self.alpha), range(1, self.size + 1))
+        )
+        pdf_sum = math.fsum(pdf)
+        self.distribution = list(map(lambda x: x / pdf_sum, pdf))
+
 
         self.beta_grad = (1 - self.beta_zero) / (self.total_steps - self.learn_start)
-
-    def build_distributions(self):
-        """
-        preprocess pow of rank
-        (rank i) ^ (-alpha) / sum ((rank i) ^ (-alpha))
-        :return: distributions, dict
-        """
-        res = {}
-        n_partitions = self.partition_num
-        partition_num = 1
-        # each part size
-        partition_size = int(math.floor(self.size / n_partitions))
-
-        for n in range(partition_size, self.size + 1, partition_size):
-            if self.learn_start <= n <= self.priority_size:
-                distribution = {}
-                # P(i) = (rank i) ^ (-alpha) / sum ((rank i) ^ (-alpha))
-                pdf = list(
-                    map(lambda x: math.pow(x, -self.alpha), range(1, n + 1))
-                )
-                pdf_sum = math.fsum(pdf)
-                distribution['pdf'] = list(map(lambda x: x / pdf_sum, pdf))
-                # split to k segment, and than uniform sample in each k
-                # set k = batch_size, each segment has total probability is 1 / batch_size
-                # strata_ends keep each segment start pos and end pos
-                cdf = np.cumsum(distribution['pdf'])
-                strata_ends = {1: 0, self.batch_size + 1: n}
-                step = 1 / self.batch_size
-                index = 1
-                for s in range(2, self.batch_size + 1):
-                    while cdf[index] < step:
-                        index += 1
-                    strata_ends[s] = index
-                    step += 1 / self.batch_size
-
-                distribution['strata_ends'] = strata_ends
-
-                res[partition_num] = distribution
-
-            partition_num += 1
-
-        return res
 
     def fix_index(self):
         """
@@ -157,24 +119,20 @@ class Experience(object):
             sys.stderr.write('Record size less than learn start! Sample failed\n')
             return False, False, False
 
-        dist_index = max(1.0, math.floor(self.record_size / self.size * self.partition_num))
-        # issue 1 by @camigord
-        partition_size = math.floor(self.size / self.partition_num)
-        partition_max = dist_index * partition_size
-        distribution = self.distributions[dist_index]
+
+        distribution = self.distribution
         rank_list = []
         # sample from k segments
         for n in range(1, self.batch_size + 1):
-            index = random.randint(min(distribution['strata_ends'][n] + 1,  distribution['strata_ends'][n + 1]),
-                                   max(distribution['strata_ends'][n] + 1,  distribution['strata_ends'][n + 1]))
+            index = random.randint(1, self.priority_queue.size + 1)
             rank_list.append(index)
 
         # beta, increase by global_step, max 1
         beta = min(self.beta_zero + (global_step - self.learn_start - 1) * self.beta_grad, 1)
         # find all alpha pow, notice that pdf is a list, start from 0
-        alpha_pow = [distribution['pdf'][v - 1] for v in rank_list]
+        alpha_pow = [distribution[v - 1] for v in rank_list]
         # w = (N * P(i)) ^ (-beta) / max w
-        w = np.power(np.array(alpha_pow) * partition_max, -beta)
+        w = np.power(np.array(alpha_pow) * self.size, -beta)
         w_max = max(w)
         w = np.divide(w, w_max)
         # rank list is priority id
